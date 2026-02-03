0ad39c59adc031de141c00c71fa9461a7ca396f7
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-02-03 16:47:19 +0530

    Program to print my name.

---
+++ 02FirstCppProgram/PrintName.cpp
@@ -0,0 +1,17 @@
+// A c++ program to print my name 10 times.
+
+#include <iostream>
+
+int main() {
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    std::cout << "My Name is Ayan Khan" << std::endl;
+    return 0;
+}
