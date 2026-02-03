1e9f8533bac9825adf0add6071ee26433b81ca8d
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-02-03 16:55:43 +0530

    Learned about single and multi-line comments.

---
+++ 03Comments/02.MultiLineComments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/*
+    This is a block comments that englobes multiple
+    line of text.
+*/
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
