fb454ecc84a4caa73f64666ed7f9587f1e03d86a
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-02-03 19:02:20 +0530

    Simple program to demonstrate the concept of Statements.

---
+++ 04Statements/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int firstNumber = 10; // This is a basic assignment statement.
+    int secondNumber = 20;
+
+    int sum = firstNumber + secondNumber; // This is a basic calculation statement.
+
+    std::cout << sum << std::endl; // This is a basic printing statement.
+    return 0;
+}
