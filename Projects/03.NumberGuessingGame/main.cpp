6cedededcaae002590479fdb7e30109fb51c871d
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-02-10 21:02:28 +0530

    Added the game loop and the core logic of game.

--- Projects/03.NumberGuessingGame/main.cpp
+++ Projects/03.NumberGuessingGame/main.cpp
@@ -8,11 +8,24 @@ int main() {
     std::uniform_int_distribution<> dist(1, 10);    
     int x = dist(gen);
    
-    // Prompt the user
-    int number;
-    std::cout << "Enter a Number (1 - 100): ";
-    std::cin >> number;
-    std::cout << std::endl;        
+    while(true) {
+        // Prompt the user
+        int number;
+        std::cout << "Enter a Number (1 - 100): ";
+        std::cin >> number;
+        std::cout << std::endl;
+
+        // Game loop
+        if (number > x) {
+            std::cout << "Too big! Try a smaller number." << std::endl;
+        } else if (number < x) {
+            std::cout << "Too small, try a larger number." << std::endl;
+        } else {
+            std::cout << "Congratulations! you found the target number." << std::endl;
+            std::cout << "Thank you for playing!" << std::endl;
+            break;
+        }
+    }  
 
     return 0;
 }
