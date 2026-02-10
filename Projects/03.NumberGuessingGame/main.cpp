c600d50452c809cdaeecd00e655cafd6d8347fee
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-02-10 20:55:21 +0530

    Added Random Number generation Mechanism

--- Projects/03.NumberGuessingGame/main.cpp
+++ Projects/03.NumberGuessingGame/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <random>
 
 int main() {
+    // Random number generation
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 10);    
+    int x = dist(gen);
+   
+    // Prompt the user
     int number;
+    std::cout << "Enter a Number (1 - 100): ";
     std::cin >> number;
+    std::cout << std::endl;        
 
     return 0;
 }
